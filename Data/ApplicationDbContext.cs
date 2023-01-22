dd201757813245bc711c43ea7f7600757b718951
MykolaMartynyuk <91531687+MykolaMartynyuk@users.noreply.github.com>
2023-01-22 17:40:05 +0100

    1

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,31 +12,26 @@ namespace Bookstore.Data
 		}
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			//modelBuilder.Entity<Book>()
-			//	.HasOne(x => x.Author)
-			//	.WithMany(x => x.Books)
-			//	.OnDelete(DeleteBehavior.S);
+			modelBuilder.Entity<Book>()
+				.HasOne(x => x.Genres)
+				.WithMany()
+				.OnDelete(DeleteBehavior.Restrict);
 
-			//modelBuilder.Entity<Book>()
-			//	.HasOne(x => x.Genre)
-			//	.WithMany()
-			//	.OnDelete(DeleteBehavior.Restrict);
+			modelBuilder.Entity<Author>()
+				.HasMany(x => x.Book)
+				.WithOne(x => x.Author)
+				.OnDelete(DeleteBehavior.Cascade);
 
-			//modelBuilder.Entity<Author>()
-			//	.HasMany(x => x.Books)
-			//	.WithOne(x => x.Author)
-			//	.OnDelete(DeleteBehavior.Cascade);
-
-			//modelBuilder.Entity<Genre>().HasData(
-			//	new Genre { Id = 1, Name = "Thriller" },
-			//	new Genre { Name = "Horror", Id = 2 },
-			//	new Genre { Id = 3, Name ="Science"},
-			//	new Genre { Id = 4, Name="Detective"},
-			//	new Genre { Id = 5, Name = "Fantasy"}
-			//	);
+			modelBuilder.Entity<Genre>().HasData(
+				new Genre { Id = 1, Name = "Thriller" },
+				new Genre { Id = 2, Name = "Horror" },
+				new Genre { Id = 3, Name = "Science" },
+				new Genre { Id = 4, Name = "Detective" },
+				new Genre { Id = 5, Name = "Fantasy" }
+				);
 
 			//modelBuilder.Entity<Book>().HasData(
-			//	new Book { AuthorId = 3, Format = (Cover)1, Title = "q", GenreId = 1, Id = 1, ImageURL = "1", ISBN13 = "q", Pages = 1, Price = 1 }
+			//	new Book { AuthorFK = 1, Format = (Cover)1, Title = "test", GenreId = 1, Id = 1, ImageURL = "1", ISBN13 = "test", Pages = 1, Price = 1 }
 			//	);
 
 			base.OnModelCreating(modelBuilder);
